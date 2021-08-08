e1668e5504085702a3d0ba750be304478a0bf998
nilsonsasaki <nilsonsasaki@gmail.com>
2021-08-08 08:21:08 -0300

    added fun calculateTip

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -1,11 +1,9 @@
 package com.example.tiptime
 
-import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.Editable
-import android.widget.Toast
 import com.example.tiptime.databinding.ActivityMainBinding
+import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,5 +14,26 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.calculateButton.setOnClickListener{calculateTip()}
+
+    }
+
+    private fun calculateTip() {
+        val stringInTextField: String = binding.costOfService.text.toString()
+        val cost = stringInTextField.toDouble()
+        val selectedId = binding.tipOptions.checkedRadioButtonId
+
+        val tipPercentage = when (selectedId){
+            R.id.option_twenty_percent->0.20
+            R.id.option_eighteen_percent->0.18
+            else->0.15
+        }
+        var tip = cost*tipPercentage
+        val roundUp = binding.roundUpSwitch.isChecked
+        if (roundUp){
+            tip = kotlin.math.ceil(tip)
+        }
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+        binding.tipResult.text = getString(R.string.tip_amount,formattedTip)
     }
 }
